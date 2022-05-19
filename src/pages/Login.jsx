b33832ffeb177ad36f717db2e74ea4bc5e9099e1
Adalberto Ramos Ribeiro <beto1821@pop-os.localdomain>
2022-05-18 21:59:50 -0300

    req02

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,66 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  state = {
+    name: '',
+    isDisable: true,
+    isLoading: false,
+  };
+
+  handleClick = async () => {
+    const { name } = this.state;
+    const { history } = this.props;
+    this.setState({ isLoading: true });
+    await createUser({ name });
+    history.push('search');
+  }
+
+  valiHandle = () => {
+    const { name } = this.state;
+    const number = 3;
+    if (name.length < number) {
+      return this.setState({ isDisable: true });
+    }
+    return this.setState({ isDisable: false });
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, () => this.valiHandle());
+  };
+
   render() {
+    const { isDisable, name, isLoading } = this.state;
     return (
       <div data-testid="page-login">
-        <h2>Login</h2>
+        { isLoading ? <p>Carregando...</p> : (
+          <form>
+            <input
+              name="name"
+              value={ name }
+              type="text"
+              placeholder="Nome"
+              data-testid="login-name-input"
+              onChange={ this.handleChange }
+            />
+            <button
+              type="button"
+              data-testid="login-submit-button"
+              disabled={ isDisable }
+              onClick={ this.handleClick }
+            >
+              Entrar
+            </button>
+          </form>
+        )}
       </div>
     );
   }
 }
+Login.propTypes = {
+  history: PropTypes.string.isRequired,
+};
 
 export default Login;
